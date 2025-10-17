800f46ec36c08c5fe5e032254cb2cfceb56d8d0d
julbar <juli.bark@gmail.com>
2025-10-17 16:18:13 +0200

    Add select component docs

--- docs/api-table.js
+++ docs/api-table.js
@@ -2627,6 +2627,22 @@ export const android = {
 
     ],
   },
+  Select: {
+    required: [
+      ['value', 'String', '', 'The text displayed currently in the select component'],
+      ['onValueChange', '(String) -> Unit', '', 'Lambda to be invoked when input value changes'],
+    ],
+    props: [
+      ['modifier', 'Modifier', 'Modifier', 'Modifier for the select component'],
+      ['label', 'String', 'null', 'The text to display as the label above the select component'],
+      ['enabled', 'Boolean', 'true', 'Sets the select component in disabled mode when false'],
+      ['readOnly', 'Boolean', 'false', 'Sets the select component in read-only mode'],
+      ['placeholderText', 'String', 'null', 'Text hint that occupies the select component when it is empty'],
+      ['helpText', 'String', 'null', 'The text to display as the help text below the select component'],
+      ['isError','Boolean','false','Renders the select component in an invalid state'],
+      ['items', 'List<String>?', 'null', 'The list of strings displayed in the select component menu']
+    ],
+  },
 };
 
 export const iOS = {
