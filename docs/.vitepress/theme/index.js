939251731f909662d6d5e82be5fa8c5b3f5c3e88
Prash Mehra <imprashast@gmail.com>
2026-01-07 15:08:06 +0100

    Cleanup more

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -10,6 +10,7 @@ import QrColorTable from '../qr-color-table.vue';
 import QrTable from '../qr-table.vue';
 import ThemeSwitcher from '../ThemeSwitcher.vue';
 import '../bootExamples.js';
+// Only import client side
 import '../StyleIsolate.js'; // Web component for style isolation
 import './custom.css';
 import 'uno.css';
@@ -19,11 +20,7 @@ import Cards from '../Cards.vue';
 import DsOverview from '../DsOverview.vue';
 import FrameworkTabs from '../FrameworkTabs.vue';
 import warpThemeSwitcher from '../warp-theme-switcher.js';
-import DsBetaNotice from './components/DsBetaNotice.vue';
-import DsCodeTabs from './components/DsCodeTabs.vue';
 import DsComponentOverview from './components/DsComponentOverview.vue';
-import DsMainTabs from './components/DsMainTabs.vue';
-import DsSearchInput from './components/DsSearchInput.vue';
 
 export default {
   extends: DefaultTheme,
@@ -38,28 +35,15 @@ export default {
     // plugins
     app.use(warpThemeSwitcher);
 
-    // Ds-prefixed components (explicitly registered)
-    //app.component('DsBadge', DsBadge);
-    //app.component('DsBetaNotice', DsBetaNotice);
-    //app.component('DsCodeTabs', DsCodeTabs);
     app.component('DsComponentOverview', DsComponentOverview);
-    //app.component('DsComponentStatus', DsComponentStatus);
-    //app.component('DsFrameworkStatus', DsFrameworkStatus);
-    //app.component('DsMainTabs', DsMainTabs);
-    //app.component('DsSearchInput', DsSearchInput);
-
-    //app.component('ApiTable', ApiTable);
     app.component('ThemeSwitcher', ThemeSwitcher);
-    //app.component('TabsContent', TabsContent);
     app.component('ComponentDesignGuidelines', ComponentDesignGuidelines);
     app.component('ComponentQuestions', ComponentQuestions);
     app.component('ComponentsStatus', ComponentsStatus);
     app.component('Do', Do);
     app.component('DoDont', DoDont);
-    // app.component('WidthController', WidthController);
     app.component('QrTable', QrTable);
     app.component('QrColorTable', QrColorTable);
-    //app.component('IconStarFull32', IconStarFull32);
     app.component('Cards', Cards);
     app.component('Card', Card);
     app.component('FrameworkTabs', FrameworkTabs);
