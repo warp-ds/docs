d4cfd6fea59d99c43a3fdb24cc2277e34673c548
Richard Walker <richard.walker@m10s.io>
2026-01-13 08:09:46 +0100

    docs(switch): fix incorrect default value

--- docs/api-table.js
+++ docs/api-table.js
@@ -1949,7 +1949,7 @@ export const elements = {
       ['name', 'string', '', 'Name used when submitting an HTML form.'],
       ['value', 'string', '', 'Value submitted when the switch is checked.'],
       ['checked', 'boolean', 'false', 'Whether the switch is on (checked).'],
-      ['disabled', 'boolean', 'true', 'Whether the switch is disabled.'],
+      ['disabled', 'boolean', 'false', 'Whether the switch is disabled.'],
     ],
     events: [
       [
