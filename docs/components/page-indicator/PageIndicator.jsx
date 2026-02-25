313ec6233ccabd5721a9e97f0bf85de26c929056
Prash Mehra <imprashast@gmail.com>
2026-02-25 10:52:46 +0100

    refactor: remove deprecated component examples and unused dependencies from documentation

--- docs/components/page-indicator/PageIndicator.jsx
+++
@@ -1,6 +0,0 @@
-import { PageIndicator } from '@warp-ds/components/react/pageindicator';
-import React from 'react';
-
-export default function PageIndicatorExample() {
-  return <PageIndicator pageCount={5} selectedPage={1} />;
-}
