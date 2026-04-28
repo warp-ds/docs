f4dba3b81cc6e6546d60eb44e3c1ee0646d462f6
William Killerud <william@killerud.com>
2026-04-28 11:02:42 +0200

    Use latest Warp Elements, SWR URL

--- docs/.vitepress/headLinks.js
+++ docs/.vitepress/headLinks.js
@@ -13,14 +13,14 @@ export const headLinks = [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/dba-dk.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/~1/dba-dk.css',
     },
   ],
   [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/finn-no.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/~1/finn-no.css',
     },
   ],
   [
@@ -34,42 +34,42 @@ export const headLinks = [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/blocket-se.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/~1/blocket-se.css',
     },
   ],
   [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/neutral-com.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/~1/neutral-com.css',
     },
   ],
   [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/vend-com.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/fonts/~1/vend-com.css',
     },
   ],
   [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/finn-no.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/css/~2/tokens/finn-no.css',
     },
   ],
   [
     'link',
     {
       rel: 'stylesheet',
-      href: 'https://assets.finn.no/pkg/@warp-ds/elements/2.8.1-next.4/styles.css',
+      href: 'https://assets.finn.no/pkg/@warp-ds/elements/~2/styles.css',
     },
   ],
   [
     'script',
     {
       type: 'module',
-      src: 'https://assets.finn.no/pkg/@warp-ds/elements/2.8.1-next.4/index.js',
+      src: 'https://assets.finn.no/pkg/@warp-ds/elements/~2/index.js',
     },
   ],
   [
