8e1c7ba45e1e16a45730470f0112226e3bf71a71
William Killerud <william@killerud.com>
2026-06-01 14:32:59 +0200

    Readd llmstxt plugin
    Removed it while debugging d219ae8cc6d59b6621152d980802a3ec7d0d7d72
    and forgot to put it back.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -55,6 +55,7 @@ export default defineConfig({
   },
   vite: {
     plugins: [
+      llmstxt(),
       uno({ presets: [presetWarp({ skipResets: true })] }),
       uno({
         presets: [presetWarp(), presetDocs()],
